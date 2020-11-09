b283eff6e5a69a51e6fb486a28f64bda3e61dbc3
lotus <1504558766@qq.com>
2020-11-09 17:54:36 +0800

    nothing

--- examples/main.js
+++ examples/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import LoEditor from '../packages/index';
+import LoEditor from '../lib/lo-editor.umd.min';
+// import LoEditor from '../packages/index';
 
 Vue.use(LoEditor)
 Vue.config.productionTip = false
